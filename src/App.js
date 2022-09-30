8f7560e8fdb53590d59edb05c64823d1044f133b
Jim Maniatis <jimmy@eummena.org>
2022-09-30 05:36:08 +0300

    Added API results into bootstrap card on the search page

--- src/App.js
+++ src/App.js
@@ -8,17 +8,19 @@ import About from "./components/About";
 import NoPage from "./components/NoPage";
 
 function App() {
-  // const [searchResults, setsearchResults] = useState([]);
+  const [searchResults, setsearchResults] = useState([]);
   const [searchText, setsearchText] = useState("");
 
   useEffect(() => {
-    fetch(
-      `https://api.themoviedb.org/3/search/movie?api_key=46bdc290ee5b68255e0fc905786d4514&language=en-US&query=${searchText}&page=1&include_adult=false`
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-      });
+    if (searchText) {
+      fetch(
+        `https://api.themoviedb.org/3/search/movie?api_key=46bdc290ee5b68255e0fc905786d4514&language=en-US&query=${searchText}&page=1&include_adult=false`
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          setsearchResults(data.results);
+        });
+    }
   }, [searchText]);
 
   return (
@@ -36,10 +38,7 @@ function App() {
           <Route
             path="search"
             element={
-              <SearchItems
-                keyword={searchText}
-                //searchResults={searchResults}
-              />
+              <SearchItems keyword={searchText} searchResults={searchResults} />
             }
           />
         </Route>
--- src/components/SearchItems.js
+++ src/components/SearchItems.js
@@ -1,11 +1,45 @@
 import React from "react";
 import Header from "./Header";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
 
+const MovieCard = ({ movie }) => {
+  const posterUrl = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
+
+  return (
+    <>
+      <Card
+        style={{
+          width: "25rem",
+          top: "20px",
+          marginRight: "20px",
+          marginLight: "20px",
+          marginBottom: "20px",
+          float: "left",
+        }}
+      >
+        <Card.Img variant="top" src={posterUrl} class="overflow-hidden" />
+        <Card.Body>
+          <Card.Title>{movie.original_title}</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+          <Button variant="primary">Go somewhere</Button>
+        </Card.Body>
+      </Card>
+    </>
+  );
+};
 const SearchItems = ({ keyword, searchResults }) => {
   const title = keyword;
+  const searchResultsHtml = searchResults.map((object, i) => {
+    return <MovieCard movie={object} key={i} />;
+  });
   return (
     <>
       <Header text={title} />
+      {searchResultsHtml}
     </>
   );
 };
